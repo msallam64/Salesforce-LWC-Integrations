f3efc2fd8b54298f2d1a9b760de483d3ee9d6fed
Mohamed Sallam <you@example.com>
2023-10-06 15:43:46 +0200

    Some Comments

--- force-app/main/default/lwc/first/first.js
+++ force-app/main/default/lwc/first/first.js
@@ -1,7 +1,17 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class First extends LightningElement {
     name = 'Mohamed';
     company = 'Marakez';
     salary = '500';
+
+    // Create lwc properties - add api first.
+    @api message // Private Property - example hold record ID
+    x;           // Public Property - Inside LWC Only  
+
+    @api total = 30;
+
+    get totalValue(){
+        return this.total;
+    }
 }
