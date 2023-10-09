f5f8c44d3477a2e7e884ed5bb806bec8008c68e0
Mohamed Ali <msallam963963@gmail.com>
2023-10-09 19:54:05 +0300

    Initi Of WireAdapter LWC Co-authored-by: Mohamed A. Sallam <mohamedalimohamed440@gmail.com>

---
+++ force-app/main/default/lwc/wireAdapterDemo/wireAdapterDemo.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+
+export default class WireAdapterDemo extends LightningElement {
+
+}
