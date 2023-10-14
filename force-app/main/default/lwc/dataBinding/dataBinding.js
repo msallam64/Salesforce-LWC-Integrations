d500d9a5407fd2bb500fcdb925e8ada7224beaf5
Mohamed Ali <msallam963963@gmail.com>
2023-10-14 15:29:30 +0300

    Using Getter To Fetch Data Co-authored-by: Mohamed A. Sallam <mohamedalimohamed440@gmail.com>

--- force-app/main/default/lwc/dataBinding/dataBinding.js
+++ force-app/main/default/lwc/dataBinding/dataBinding.js
@@ -8,4 +8,31 @@ export default class DataBinding extends LightningElement {
     handleChange(event){
         this.message = event.target.value;
     }
+
+    // For Fetch Data Using Getter Demo 
+    firstName = '';
+    lastName = '';
+
+    // 1- Function to Handle changing
+    fullNameHandleChange(event){
+
+        // That Var for check which target name (fname OR lname)
+        const field = event.target.name;
+
+        // Check which one
+        if(field == 'fname'){
+            this.firstName = event.target.value;
+        }else if (field == 'lname'){
+            this.lastName = event.target.value;
+        }
+    }
+
+    // 2- Using Get to update fullNameUpperCase in HTML
+    // return value will automatically go to property in HTML file
+    get fullNameUpperCase(){
+
+        // Return First Name & last Name in uppercase
+        return `${this.firstName} ${this.lastName}`.toUpperCase();
+
+    }
 }
