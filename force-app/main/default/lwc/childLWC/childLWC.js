4c3106cdab1b1091a05549b621510be0cb6b3ba6
Mohamed Ali <msallam963963@gmail.com>
2023-10-16 18:07:46 +0300

    LWC Parent & Child Components Demo

---
+++ force-app/main/default/lwc/childLWC/childLWC.js
@@ -0,0 +1,17 @@
+import { LightningElement, api } from 'lwc';
+
+export default class ChildLWC extends LightningElement {
+
+    upperCaseItemName = 'Default Value';
+
+    // Getter - public property because used in another component
+    @api 
+    get itemName(){
+        return this.upperCaseItemName;
+    }
+
+    // Setter - Receiving Value and set it to upperCaseItemName
+    set itemName(value){
+        this.upperCaseItemName = value.toUpperCase();
+    }
+}
---
+++ force-app/main/default/lwc/parentLWC/parentLWC.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+import childLWC from 'c/childLWC';
+
+
+export default class ParentLWC extends LightningElement {}
